b69474a8ece3fc17db0b21788bc48dc715036bf9
JingZ-lab <jiz403@ucsd.edu>
2026-08-27 21:35:17 +0000

    wrote py file

---
+++ Aitraining.py
@@ -0,0 +1,40 @@
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+import pandas as pd
+food = pd.read_csv("food.csv")
+X = food["calories", "protein", "sodium", "fat", "carbs", "vegetarian","allergens","Vegetarian","Vagen"]
+y = food["liked"]
+
+print(X.shape)
+print(y.shape)
+X = food.data.features
+y = food.data.targets
+print(food .metadata)
+
+# Logistic Regression
+
+favourite_prediction_accuracies = []
+
+food_train, food_test, favourite_train, favourite_test = train_test_split(
+    food_features,
+    favourite_labels,
+    test_size=0.2,
+    random_state=5
+    )
+
+  
+scaler = StandardScaler()
+food_train = scaler.fit_transform(food_train)
+food_test = scaler.transform(food_test)
+food_preference_model = LogisticRegression(max_iter=1000)
+food_preference_model.fit(food_train, favourite_train)
+prediction_accuracy = food_preference_model.score(
+        food_test,
+        favourite_test
+)
+
+favourite_prediction_accuracies.append(prediction_accuracy)
+
+print(np.mean(favourite_prediction_accuracies))
