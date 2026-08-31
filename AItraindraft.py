588ad6c7f3c73be7b8941175b1f445ba9d160a81
JingZ-lab <jiz403@ucsd.edu>
2026-08-31 21:48:04 +0000

    basic binary function complete

--- AItraindraft.py
+++ AItraindraft.py
@@ -4,7 +4,7 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score 
 import pandas as pd
 import numpy as np
-
+from binaryrecomendfoodfunction import wantcalories,wantfat,wantprotein,wantCholesterol,wantSodium,wantCarbs,wantFiber,wantSugar,hasAllergy
 # make a list
 food = pd.read_csv("foods.csv")
 menu = []
@@ -13,88 +13,64 @@ for name in food["Name"]:
     menu.append(name)
 for foodname in menu: 
     ratemyfood[foodname] = 0
+
+#basic set up
+food["Fat"] = pd.to_numeric(food["Fat"], errors="coerce")
+food["Fat"] = food["Fat"].fillna(0)
+food["Calories"] = pd.to_numeric(food["Calories"], errors="coerce")
+food["Calories"] = food["Calories"].fillna(0)
+food["Cholesterol"] = pd.to_numeric(food["Cholesterol"], errors="coerce")
+food["Cholesterol"] = food["Cholesterol"].fillna(0)
+food["Sodium"] = pd.to_numeric(food["Sodium"], errors="coerce")
+food["Sodium"] = food["Sodium"].fillna(0)
+food["Carbs"] = pd.to_numeric(food["Carbs"], errors="coerce")
+food["Carbs"] = food["Carbs"].fillna(0)
+food["Fiber"] = pd.to_numeric(food["Fiber"], errors="coerce")
+food["Fiber"] = food["Fiber"].fillna(0)
+food["Sugar"] = pd.to_numeric(food["Sugar"], errors="coerce")
+food["Sugar"] = food["Sugar"].fillna(0)
 food["Protein"] = pd.to_numeric(food["Protein"], errors="coerce")
 food["Protein"] = food["Protein"].fillna(0)
+
+
 food_unique = food.drop_duplicates(subset=["Name"])
 
-allergy = input("Are you allergic to any? please type: dairy/ eggs / gluten / soy / wheat / sesame / fish/ no?")
-for index, row in food_unique.iterrows():
-    name = row["Name"]
-    if row["Allergy: dairy?"] == "yes" and allergy == "dairy":
-        del ratemyfood[name]
-    if row["Allergy: eggs?"]  == "yes" and allergy == "egg":
-        del ratemyfood[name]
-    if row["Allergy: gluten?"]  == "yes" and allergy == "gluten":
-        del ratemyfood[name]
-    if row["Allergy: soy?"]  == "yes" and allergy == "soy":
-        del ratemyfood[name]
-    if row["Allergy: wheat?"]  == "yes" and allergy == "wheat":
-        del ratemyfood[name]
-    if row["Allergy: sesame?"]  == "yes" and allergy == "sesame":
-        del ratemyfood[name]
-    if row["Allergy: fish?"] == "yes" and allergy == "fish":
-        del ratemyfood[name]
-    else: 
-        continue
-for name1 in ratemyfood:
-    print(name1,ratemyfood[name1])
+Allergytype = (input("do you have any allergy?tell it by yes/no")).lower()
+while Allergytype == "yes":
+    hasAllergy(ratemyfood,food_unique) 
+    Allergytype = (input("do you have any allergy?tell it by yes/no")).lower()
+
+
 # print(ratemyfood)
-
-
-proteinchoice = input("Do you want high protein? yes/no: ")
-Proteinlist = [] 
-if proteinchoice == "yes":
-    score = len(ratemyfood)
-    while score > 0:
-        highest_protein = -1
-        highest_food = " "
-        for index, row in food_unique.iterrows():
-            if row["Name"] not in Proteinlist and row["Name"] in ratemyfood:
-                if int(row["Protein"]) >= highest_protein:
-                    highest_protein = int(row["Protein"])
-                    highest_food = row["Name"]
-        Proteinlist.append(highest_food)
-        ratemyfood[highest_food] += score
-        score -= 1
 # print("after protein")
 # print(ratemyfood["Turkey Meatball Sub"])
 
-calorieschoice = input("Do you want high calories? yes/no: ")
-calorieslist = [] 
-if calorieschoice == "yes":
-    score = len(ratemyfood)
-    while score > 0:
-        highest_calories = -1
-        highest_food = " "
-        for index, row in food_unique.iterrows():
-            if row["Name"] not in calorieslist and row["Name"] in ratemyfood:
-                if int(row["Calories"]) >= highest_calories:
-                    highest_calories = int(row["Calories"])
-                    highest_food = row["Name"]
-        calorieslist.append(highest_food)
-        ratemyfood[highest_food] += score
-        score -= 1
-
-Fatchoice = input("Do you want high calories? yes/no: ")
-Fatlist = [] 
-if Fatchoice == "yes":
-    score = len(ratemyfood)
-    while score > 0:
-        highest_fats = -1
-        highest_food = " "
-        for index, row in food_unique.iterrows():
-            if row["Name"] not in Fatlist and row["Name"] in ratemyfood:
-                if int(row["Fat"]) >= highest_fats:
-                    Fates = int(row["Fat"])
-                    highest_food = row["Name"]
-        Fatlist.append(highest_food)
-        ratemyfood[highest_food] += score
-        score -= 1
-
 #test method
-for name1 in ratemyfood:
-    print(name1,ratemyfood[name1])
+# for name1 in ratemyfood:
+#     print(name1,ratemyfood[name1])
 # print(ratemyfood)
+
+wantcalories(ratemyfood,food_unique)
+wantfat(ratemyfood,food_unique)
+wantprotein(ratemyfood,food_unique)
+wantCholesterol(ratemyfood,food_unique)
+wantSodium(ratemyfood,food_unique)
+wantCarbs(ratemyfood,food_unique)
+wantFiber(ratemyfood,food_unique)
+wantSugar(ratemyfood,food_unique)
+
+topchoice = []
+for i in range(5):
+    highest_score = -1
+    highest_food = ""
+    for name in ratemyfood:
+        if name not in topchoice:
+            if ratemyfood[name] > highest_score:
+                highest_score = ratemyfood[name]
+                highest_food = name
+    topchoice.append(highest_food)
+
+print(topchoice)
 
 
 
