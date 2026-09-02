442d1489f19aee0e9d1d549565ba8b32049cb0cb
Carrie Xu <carrielsxu@gmail.com>
2026-09-02 21:57:36 +0000

    allow github pages to connect to flask

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify, send_file
+from flask_cors import CORS
 from ml import get_recommendation
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/")
 def home():
